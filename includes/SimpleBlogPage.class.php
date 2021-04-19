53d3e4522027a513f27093fde57641e431670cbd
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-04-19 12:23:44 -0600

    Update SimpleBlogPage.class.php

--- includes/SimpleBlogPage.class.php
+++ includes/SimpleBlogPage.class.php
@@ -20,46 +20,7 @@ class SimpleBlogPage extends Article {
 
 	public function __construct( Title $title ) {
 		parent::__construct( $title );
-		$this->setContent();
 		$this->getAuthor();
-	}
-
-	public function setContent() {
-		// Get the page content for later use
-		$this->pageContent = ContentHandler::getContentText(
-			$this->getContentObject()
-		);
-
-		// If it's a redirect, in order to get the *real* content for later use,
-		// we have to load the text for the real page
-		// Note: If $this->getContent() is called anywhere before parent::view,
-		// the real article text won't get loaded on the page
-		if ( $this->getTitle()->isRedirect() ) {
-			wfDebugLog( 'SimpleBlogPage', __METHOD__ );
-
-			$target = WikiPage::factory( $this->getTitle() )->followRedirect();
-			if ( !$target instanceof Title ) {
-				// Correctly handle interwiki redirects and like
-				// WikiPage::followRedirect() can return either a Title, boolean
-				// or a string! A string is returned for interwiki redirects,
-				// and the string in question is the target URL with the rdfrom
-				// URL parameter appended to it, which -- since it's an interwiki
-				// URL -- won't resolve to a valid local Title.
-				// Doing the redirection here is somewhat hacky, but ::getAuthors(),
-				// which is called right after this function in the constructor,
-				// attempts to read $this->pageContent...
-				// @see https://github.com/Brickimedia/brickimedia/issues/370
-				$this->getContext()->getOutput()->redirect( $target );
-			} else {
-				$rarticle = new Article( $target );
-				$rcontent = $rarticle->getContentObject();
-				$this->pageContent = ContentHandler::getContentText( $rcontent );
-
-				// If we don't clear, the page content will be [[redirect-blah]],
-				// and not the actual page
-				$this->clear();
-			}
-		}
 	}
 
 	/**
@@ -67,8 +28,7 @@ class SimpleBlogPage extends Article {
 	 */
 	function getAuthor() {
 		$this->AuthorName = Title::newFromText( $this->getTitle()->getText() )->getRootText();
-		$authorObj = User::newFromName( $this->AuthorName );
-		// $this->AuthorID = $authorObj->getActorId(); //DO NOT USE getActorId() here!!! IT GIVES THE WRONG ID. YOU WANT getId() INSTEAD!! 
+		$authorObj = User::newFromName( $this->AuthorName ); 
 		$this->AuthorID = $authorObj->getId();
 	}
 
@@ -227,7 +187,7 @@ class SimpleBlogPage extends Article {
 		$edit_time['datetime'] = $lang->timeanddate( $timestamp, true );
 
 		// Get date of when article was created
-		$timestamp = self::getCreateDate( $this->getId() );
+		$timestamp = self::getCreateDate( WikiPage::factory( $this->getTitle() )->getID() );
 		$create_time = [];
 		$create_time['date'] = $lang->date( $timestamp, true );
 		$create_time['time'] = $lang->time( $timestamp, true );
@@ -271,7 +231,7 @@ class SimpleBlogPage extends Article {
 	 * @return array Array containing each editors' user ID and user name
 	 */
 	public function getEditorsList() {
-		$pageTitleId = $this->getId();
+		$pageTitleId = WikiPage::factory( $this->getTitle() )->getID();
 		$editors = [];
 
 		wfDebugLog( 'SimpleBlogPage', "Loading recent editors for page {$pageTitleId} from DB" );
