295ff3f3ffe16b4939cc24316abe71311b9624b9
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-04-19 19:32:04 -0600

    Fix WikiEditor integration

--- includes/specials/SpecialCreateBlogPost.php
+++ includes/specials/SpecialCreateBlogPost.php
@@ -24,8 +24,7 @@ class SpecialCreateBlogPost extends FormSpecialPage {
 		$out->addModuleStyles( 'ext.blogPage.create.css' );
 
 		if ( ExtensionRegistry::getInstance()->isLoaded( 'WikiEditor' ) && $this->getContext()->getUser()->getOption( 'usebetatoolbar' ) ) {
-			$out->addModuleStyles( 'ext.wikiEditor.styles' );
-			$out->addModules( 'ext.wikiEditor' );
+			$out->addModules( [ 'ext.wikiEditor', 'ext.AddMoreButton' ] );
 		}
 
 		$cloud = new BlogTagCloud( 1000 );
